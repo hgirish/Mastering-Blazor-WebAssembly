2d88078863ca255e460d1e3d0c74e7790464b779
Girish <girish@post.com>
2023-09-13 21:21:56 -0700

    navigation to details

--- BooksStore/BooksStore/Models/Book.cs
+++ BooksStore/BooksStore/Models/Book.cs
@@ -2,7 +2,9 @@
 
 public class Book
 {
+    public string? Id { get; set; } = Guid.NewGuid().ToString();
     public string? Title { get; set; }
     public string? AuthorName { get; set; }
     public DateTime PublishingDate { get; set; }
+    public string? Description { get; set; }
 }
--- BooksStore/BooksStore/Services/LocalBooksService.cs
+++ BooksStore/BooksStore/Services/LocalBooksService.cs
@@ -8,25 +8,38 @@ public class LocalBooksService : IBooksService
     {
         new Book
         {
+            
             AuthorName = "John Smith",
             PublishingDate = new DateTime(2021, 01, 12),
-            Title = "Blazor WebAssembly Guide"
+            Title = "Blazor WebAssembly Guide",
+            Description = "Blazor WebAssembly Guide by John Smith."
         },
          new Book
         {
+            
             AuthorName = "John Smith",
             PublishingDate = new DateTime(2022, 02, 13),
-            Title = "Mastering Blazor WebAssembly"
+            Title = "Mastering Blazor WebAssembly",
+            Description =  "Mastering Blazor WebAssembly by John Smith"
         },
           new Book
         {
+             
             AuthorName = "John Smith",
             PublishingDate = new DateTime(2021, 08, 01),
-            Title = "Learning Blazor from A to Z"
+            Title = "Learning Blazor from A to Z",
+            Description = "Learning Blazor from A to Z by John Smith"
         },
     };
     public Task<List<Book>> GetAllBooksAsync()
     {
         return Task.FromResult(_allBooks);
+    }
+
+    public Task<Book?> GetBookByIdAsync(string? id)
+    {
+        var book = _allBooks.SingleOrDefault(b=> b.Id  == id);
+        return Task.FromResult(book);
+        
     }
 }
