04a46e4d30a255e485f559ad9800ea34ff23394f
Girish <girish@post.com>
2023-09-17 18:09:30 -0700

    Mark down

---
+++ BooksStore/BooksStore.Blazor.Components/wwwroot/blazor-simplemde.js
@@ -0,0 +1,17 @@
+﻿var simplemde;
+
+function initializeMarkdownEditor() {
+    simplemde = new SimpleMDE();
+}
+
+function getMarkdownEditorValue() {
+    if (simplemde != null) {
+        return simplemde.value();
+    }
+    return '';
+}
+function setMarkdownEditorValue(value) {
+    if (simplemde != null) {
+        simplemde.value(value);
+    }
+}
