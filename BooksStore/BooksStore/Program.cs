75b8560a624f8ce8545481354e56d16b761624c4
Girish <girish@post.com>
2023-09-21 18:03:53 -0700

    DelegatingHandler

--- BooksStore/BooksStore/Program.cs
+++ BooksStore/BooksStore/Program.cs
@@ -8,8 +8,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { 
-    BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
+builder.Services.AddScoped<DemoLoggingHandler>();
+
+//builder.Services.AddScoped(sp => new HttpClient { 
+//    BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
+builder.Services.AddHttpClient("BooksStore.API", httpClient =>
+httpClient.BaseAddress = new Uri(builder.Configuration["ApiUrl"]))
+    .AddHttpMessageHandler<DemoLoggingHandler>();
+
+builder.Services.AddScoped(sp =>
+sp.GetRequiredService<IHttpClientFactory>()
+.CreateClient("BooksStore.API"));
 
 builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
 builder.Services.AddScoped<IBooksService, LocalBooksService>();
